58fd4eadf71e4ca023662fae327ffd0e105fd96d
Иван Лаврущев <ivan_olympiad@mail.ru>
2024-11-02 17:50:14 +0300

    new errors

--- custom_asserts.h
+++ custom_asserts.h
@@ -5,20 +5,33 @@
 
 enum TYPE_OF_ERROR
 {
+    SUCCESS         = 0,
     POINTER_IS_NULL = 1,
     DOUBLE_IS_NAN   = 2,
     FILE_OPEN_ERROR = 3,
     FILE_READ_ERROR = 4,
     STAT_ERROR      = 5,
-    CALLOC_ERROR    = 6
+    CALLOC_ERROR    = 6,
+    VALUE_ERROR     = 7,
+    INPUT_ERROR     = 8,
+    PROGRAM_ERROR   = 9
 };
+
+#define warning(condition, message) do                                                     \
+{                                                                                          \
+    if(!(condition))                                                                      \
+    {                                                                                      \
+        color_printf(RED_TEXT, BOLD, "ERROR %s : %s, file: %s, function: %s, line: %d\n",  \
+        #condition, #message, __FILE__, __FUNCTION__, __LINE__);                           \
+        exit(message);                                                                     \
+    }                                                                                      \
+} while(0)
 
 #ifndef NDEBUG
 
     #define check_expression(condition, message) do                                            \
     {                                                                                          \
-        int condition_return = condition;                                                      \
-        if (!(condition_return))                                                               \
+        if(!(condition))                                                                      \
         {                                                                                      \
             color_printf(YELLOW_TEXT, BOLD, "ERROR %s : %s, file: %s, function: %s, line: %d\n",\
                         #condition, #message, __FILE__, __FUNCTION__, __LINE__);               \
@@ -27,22 +40,9 @@ enum TYPE_OF_ERROR
         }                                                                                      \
     } while(0)
 
-    #define warning(condition, message) do                                                     \
-    {                                                                                          \
-        if (!(condition))                                                                      \
-        {                                                                                      \
-            color_printf(RED_TEXT, BOLD, "ERROR %s : %s, file: %s, function: %s,                \
-                        line: %d\n", #condition, #message, __FILE__, __FUNCTION__, __LINE__);  \
-            printf("\n");                                                                      \
-            exit(0);                                                                           \
-        }                                                                                      \
-    } while(0)
-
 #else
 
     #define check_expression(condition, message)
-
-    #define warning(condition, message)
 
 #endif
 #endif
