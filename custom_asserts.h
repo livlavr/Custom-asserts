38b670463489709f21d9febe15d382da2a6c2049
Иван Лаврущев <ivan_olympiad@mail.ru>
2025-04-10 11:15:36 +0300

    [ CustomAsserts ] Changed naming

--- custom_asserts.h
+++ custom_asserts.h
@@ -3,39 +3,29 @@
 
 #include "Color-printf/color_printf.h"
 
-enum TYPE_OF_ERROR
-{
-    POINTER_IS_NULL = 1,
-    DOUBLE_IS_NAN   = 2,
-    FILE_OPEN_ERROR = 3,
-    FILE_READ_ERROR = 4,
-    STAT_ERROR      = 5,
-    CALLOC_ERROR    = 6
-};
-
 #ifndef NDEBUG
 
-    #define check_expression(condition, message) do                                            \
-    {                                                                                          \
-        int condition_return = condition;                                                      \
-        if (!(condition_return))                                                               \
-        {                                                                                      \
-            color_printf(YELLOW_TEXT, BOLD, "ERROR %s : %s, file: %s, function: %s, line: %d\n",\
-                        #condition, #message, __FILE__, __FUNCTION__, __LINE__);               \
-            printf("\n");                                                                      \
-            return message;                                                                    \
-        }                                                                                      \
+    #define warning(condition, message) do                                                      \
+    {                                                                                           \
+        int condition_return = condition;                                                       \
+        if (!(condition_return))                                                                \
+        {                                                                                       \
+            color_printf(YELLOW_COLOR, BOLD, "ERROR %s : %s, file: %s, function: %s, line: %d\n",\
+                        #condition, #message, __FILE__, __FUNCTION__, __LINE__);                \
+            printf("\n");                                                                       \
+            return message;                                                                     \
+        }                                                                                       \
     } while(0)
 
-    #define warning(condition, message) do                                                     \
-    {                                                                                          \
-        if (!(condition))                                                                      \
-        {                                                                                      \
-            color_printf(RED_TEXT, BOLD, "ERROR %s : %s, file: %s, function: %s,                \
-                        line: %d\n", #condition, #message, __FILE__, __FUNCTION__, __LINE__);  \
-            printf("\n");                                                                      \
-            exit(0);                                                                           \
-        }                                                                                      \
+    #define customAssert(condition, message) do                                                 \
+    {                                                                                           \
+        if (!(condition))                                                                       \
+        {                                                                                       \
+            color_printf(YELLOW_COLOR, BOLD, "ERROR %s : %s, file: %s, function: %s,            \
+                        line: %d\n", #condition, #message, __FILE__, __FUNCTION__, __LINE__);   \
+            printf("\n");                                                                       \
+            exit(0);                                                                            \
+        }                                                                                       \
     } while(0)
 
 #else
