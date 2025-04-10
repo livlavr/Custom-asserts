e9d54574e82b44217bf52eceadb7408c6c100f8d
Иван Лаврущев <ivan_olympiad@mail.ru>
2025-04-10 12:29:28 +0300

    [ CustomAsserts ] Submodule update

--- custom_asserts.h
+++ custom_asserts.h
@@ -1,7 +1,7 @@
 #ifndef CUSTOM_ASSERTS_H_
 #define CUSTOM_ASSERTS_H_
 
-#include "Color-printf/color_printf.h"
+#include "color_printf.h"
 
 #ifndef NDEBUG
 
@@ -16,15 +16,15 @@
         }                                                                                       \
     } while(0)
 
-    #define customAssert(condition, message) do                                                 \
-    {                                                                                           \
-        if (!(condition))                                                                       \
-        {                                                                                       \
-            color_printf(YELLOW_COLOR, BOLD, "ERROR %s : %s, file: %s, function: %s,            \
-                        line: %d\n", #condition, #message, __FILE__, __FUNCTION__, __LINE__);   \
-            printf("\n");                                                                       \
-            exit(1);                                                                            \
-        }                                                                                       \
+    #define customAssert(condition, message) do                                                   \
+    {                                                                                             \
+        if (!(condition))                                                                         \
+        {                                                                                         \
+            color_printf(YELLOW_COLOR, BOLD, "ERROR %s : %s, file: %s, function: %s, line: %d\n", \
+                         #condition, #message, __FILE__, __FUNCTION__, __LINE__);                 \
+            printf("\n");                                                                         \
+            exit(1);                                                                              \
+        }                                                                                         \
     } while(0)
 
 #else
